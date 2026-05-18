015dc4f53ce491f056bf43437e4f34441dd2db39
SanghyeokSon <51911113+SanghyeokSon@users.noreply.github.com>
2026-05-18 23:58:26 +0000

    Deploying to gh-pages from @ SanghyeokSon/SanghyeokSon.github.io@367b39cc03a79afe591f8225d2b74de743613e01 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SanghyeokSon/SanghyeokSon",
+    "data-repo": "SanghyeokSon/SanghyeokSon.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
